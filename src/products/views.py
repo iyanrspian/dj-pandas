e7bcf55c44522f663e09ab371ab8edb3861c102c
iyanrspian <iyanrispian@outlook.com>
2020-12-26 00:01:04 +0700

    add templates and show dataframe

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product, Purchase
+import pandas as pd
 
 # Create your views here.
+
+def chart_select_view(request):
+    product_df = pd.DataFrame(Product.objects.all().values())
+    purchase_df = pd.DataFrame(Purchase.objects.all().values())
+    # print(product_df)
+    # print(purchase_df)
+    context = {
+        'products' : product_df.to_html(),
+        'purchase' : purchase_df.to_html(),
+    }
+    return render(request, 'products/main.html', context)
